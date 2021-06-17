1b351652afb4ec7e8999088e5dbade907aec4d4b
David Alvarez <alvarezdavidt15@gmail.com>
2021-06-17 14:07:48 -0500

    fix: new arch

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
 
 import {
     BrowserRouter as Router,
     Switch, Route
 } from 'react-router-dom';
 
-import Welcome from '../pages/Welcome';
-import Login from '../pages/Login';
-import Signup from '../pages/Signup'
+import { Context, Provider } from '../context/Context';
 
+/* Auth Components */
+import Login from '../pages/authentication/Login';
+import Signup from '../pages/authentication/Signup';
+import ActivateAccount from '../pages/authentication/ActivateAccount';
+import ResendActivateAccount from '../pages/authentication/ResendAccountActivation';
+import SendRecoverAccount from '../pages/authentication/SendRecoverAccount';
+import RecoverAccount from '../pages/authentication/RecoverAccount';
 
-const App = () => (
+/* Admin Components */
+import Dashboard from '../pages/admin/Dashboard'
+import CorporationList from '../pages/admin/Corporation/CorporationsList';
+import newCorporation from '../pages/admin/Corporation/newCorporation'
 
-    <Router>
-        <Switch>
-            <Route exact path="/" component={Welcome} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-        </Switch>
-    </Router>
-)
+const App = () => {
+
+    const [auth, saveAuth] = useContext(Context);
+
+    return (
+        <Router>
+            <Provider value={[auth, saveAuth]}>
+                <Switch>
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/signup" component={Signup} />
+                    <Route exact path="/activate/:token" component={ActivateAccount} />
+                    <Route exact path="/send/activate" component={ResendActivateAccount} />
+                    <Route exact path="/send/recover" component={SendRecoverAccount} />
+                    <Route exact path="/recover/:token" component={RecoverAccount} />
+                    <Route exact path="/dashboard" component={Dashboard} />
+                    <Route exact path="/admin/corporation" component={CorporationList} />
+                    <Route exact path="/admin/corporation/new" component={newCorporation} />
+                    {/*<Route exact path="/corporation/:id" component={} />*/}
+                </Switch>
+            </Provider>
+        </Router>
+    )
+}
 export default App;
