fa0625146c4fe55ec53dab583dcdb331a4bafe4b
David Alvarez <alvarezdavidt15@gmail.com>
2021-06-20 21:43:47 -0500

    chore: refact name of the component to list corporations

--- src/components/dashboard/Admin/Corporation/CorporationList.js
+++ src/components/dashboard/Admin/Corporation/CorporationList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { CorporationRow } from './CorporationRow';
+import { CorporationRow } from './';
 import { apiAxios } from '../../../../config/api';
 
 export const CorporationAdminList = () => {
@@ -28,10 +28,11 @@ export const CorporationAdminList = () => {
 
 
 
-    }, [])
+    }, [])  
 
     return (
         <main>
+            {/* STYLE FOR USERS
             <div className="card-header">
                 <h2 className="header-text">Compañias registradas</h2>
 
@@ -41,11 +42,49 @@ export const CorporationAdminList = () => {
             </div>
             <div className="cards">
                 {corporation.map(corp => (
-                    <CorporationRow
+                    <CorporationCard
                         key={corp._id}
                         corp={corp}
                     />
                 ))}
+            </div>
+            */}
+
+            <div className="card">
+                <div className="card-header">
+                    <h2>Compañias registradas</h2>
+
+                    <Link to={'/admin/corporation/new'}><button className="header-button">Agregar una<span className="las-la-arrow-right">
+
+                    </span> </button></Link>
+                </div>
+                <div className="card-body">
+                    <div className="table-responsive">
+
+                        <div className="table-wrapper">
+                            <table className="fl-table">
+                                <thead>
+                                    <tr>
+                                        <th></th>
+                                        <th>Nombre</th>
+                                        <th>RIF</th>
+                                        <th>Descripción</th>
+                                        <th>Activa</th>
+                                        <th>Acciones</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {corporation.map(corp => (
+                                        <CorporationRow
+                                            key={corp._id}
+                                            corp={corp}
+                                        />
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
         </main>
     )
