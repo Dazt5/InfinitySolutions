1edfcb098a4147369604177c207eee3690475871
David Alvarez <david.alvarez@valid.com>
2021-07-14 20:53:12 -0500

    added profile to ticket (INCOMPLETED: PENDING SHOW REPLIES, SEND A REPLY)

--- src/components/dashboard/common/TicketDetail/index.js
+++ src/components/dashboard/common/TicketDetail/index.js
@@ -1,5 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
+import React, { useContext, useEffect, useState } from 'react';
 import { apiAxios } from '../../../../config/api';
+import { Context } from '../../../../context/Context';
 import gravatar from '../../../../utils/gravatar';
 import './style.css';
 
@@ -7,49 +9,85 @@ export const TicketDetail = ({ props }) => {
 
     const { idTicket } = props.match.params;
 
-    const [ticket, saveTicket] = useState({});
+    const [ticket, saveTicket] = useState({
+        corporation: {},
+        user: {
+            email: 'email@email.com'
+        },
+        status: {}
+    });
+    const [statuses, saveStatuses] = useState([]);
+
+    const [auth] = useContext(Context);
 
     useEffect(() => {
 
         const getTicket = async () => {
 
-            const request = await apiAxios.get(`/ticket/${idTicket}`);
+            try {
 
-            console.log(request)
+                const { data } = await apiAxios.get(`/ticket/${idTicket}`);
+                saveTicket(data.ticket);
 
-            saveTicket()
+            } catch (error) {
+                console.log(error);
+            }
         }
         getTicket();
-    }, [])
 
-    console.log(ticket);
+        if (auth.user.auth_level === 2) {
+            const getStatuses = async () => {
+                try {
+                    const { data } = await apiAxios.get(`/status/`);
+                    saveStatuses(data.status);
+
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+            getStatuses();
+        }
+    }, [auth])
 
     return (
         <main className="body ticket-details">
             <div className="container-fluid mt-100">
                 <div className="row">
-                    <div className="col-md-9">
+                    <div className={auth.user.auth_level && auth.user.auth_level === 2 ? "col-md-9" : "col-md-12"}>
                         <div className="card ticket-detail mb-4">
                             <div className="card-header ticket-detail">
-                                <div className="media flex-wrap w-100 align-items-center"> <img src="https://i.imgur.com/iNmBizf.jpg" className="d-block ui-w-40 rounded-circle" alt="" />
-                                    <div className="media-body ml-3"> { }<a href="" data-abc="true"> </a>
+                                <div className="media flex-wrap w-100 align-items-center"> <img src={gravatar(ticket.user.email)} className="d-block ui-w-40 rounded-circle" alt="" />
+                                    <div className="media-body ml-3"> {ticket.user.name} {ticket.user.lastname}<a href="" data-abc="true"> </a>
                                     </div>
                                     <div className="text-muted small ml-3">
-                                        <div>Member since <strong>01/1/2019</strong></div>
-                                        <div><strong>134</strong> posts</div>
+                                        <div>Ticket generado el <strong>{dayjs(ticket.create_at).format('DD-MM-YY')}</strong></div>
+                                        <div><strong>A las </strong> {dayjs(ticket.create_at).format('hh:mm A')}</div>
                                     </div>
                                 </div>
                             </div>
                             <div className="card-body ticket-detail">
-                                <p> For me, getting my business website made was a lot of tech wizardry things. Thankfully i get an ad on Facebook ragarding commence website. I get connected with BBB team. They made my stunning website live in just 3 days. With the increase demand of online customers. I had to take my business online. BBB Team guided me at each step and enabled me to centralise my work and have control on all aspect of my online business. </p>
-                                <p> For me, getting my business website made was a lot of tech wizardry things. Thankfully i get an ad on Facebook ragarding commence website. I get connected with BBB team. They made my stunning website live in just 3 days. With the increase demand of online customers. I had to take my business online. BBB Team guided me at each step and enabled me to centralise my work and have control on all aspect of my online business. </p>
+                                <h4> {ticket.subject}</h4>
+                                <p> {ticket.description} </p>
                             </div>
                             <div className="card-footer d-flex flex-wrap justify-content-between align-items-center px-0 pt-0 pb-3">
-                                <div className="px-4 pt-3"> <a href="" className="text-muted d-inline-flex align-items-center align-middle" data-abc="true"> <i className="fa fa-heart text-danger"></i>&nbsp; <span className="align-middle">445</span> </a> <span className="text-muted d-inline-flex align-items-center align-middle ml-4"> <i className="fa fa-eye text-muted fsize-3"></i>&nbsp; <span className="align-middle">14532</span> </span> </div>
-                                <div className="px-4 pt-3"> <button type="button" className="btn btn-primary"><i className="ion ion-md-create"></i>&nbsp; Reply</button> </div>
-                            </div>
+                                {
+                                    ticket.status.name === "waiting" &&
+                                    <div className="px-4 pt-3"><i className="las la-clock waitingicon"> <p> En espera</p></i></div>
+                                }
+                                {
+                                    ticket.status.name === "success" &&
+                                    <div className="px-4 pt-3"><i className="las la-check-circle checkicon"><p>Solucionado</p></i></div>
+                                }
+                                {
+                                    ticket.status.name === "reject" &&
+                                    <div className="px-4 pt-3"><i className="fa fa-check-times rejecticon"><p>Rechazado</p></i></div>
+                                }
+                                <div className="px-4 pt-3"> <button type="button" className="btn btn-primary">Reply</button> </div>
                         </div>
                     </div>
+                </div>
+                {
+                    auth.user.auth_level && auth.user.auth_level === 2 &&
                     <div className="col-md-3">
                         <div className="card ticket-detail mb-4">
                             <div className="container-form">
@@ -59,7 +97,10 @@ export const TicketDetail = ({ props }) => {
 
                                         <span>Seleccione una estado</span>
                                         <select name="corporation" >
-                                            <option value=""> Seleccione una corporación</option>
+                                            <option value="">Seleccione un estado</option>
+                                            {statuses.map(status => (
+                                                <option key={status._id} value={status._id}>{status.name}</option>
+                                            ))}
                                         </select>
 
                                     </div>
@@ -67,7 +108,8 @@ export const TicketDetail = ({ props }) => {
                             </div>
                         </div>
                     </div>
-                </div>
+                }
+            </div>
             </div>
         </main>
     )
