1eebb96cfd286b235a8784105a30fddd5f2cd7f9
David Alvarez <david.alvarez@valid.com>
2021-10-31 11:49:23 -0500

    ft: added Resume in admin dashboard

--- src/components/dashboard/Admin/AdminPanel/DashboardResume/index.js
+++ src/components/dashboard/Admin/AdminPanel/DashboardResume/index.js
@@ -2,61 +2,67 @@ import React, { Fragment, useEffect, useState } from 'react';
 import { apiAxios } from '../../../../../config/api';
 
 export const DashboardResume = () => {
-    const [Resume, saveResume] = useState({});
+    const [resume, saveResume] = useState({
+        users: 0,
+        tickets: {
+            total: 0,
+            waiting: 0,
+            success: 0
+        }
+    });
     const getResume = async () => {
-
         try {
-
             const { data } = await apiAxios.get('/admin/dashboard');
-            console.log(data);
             saveResume(data.resume);
-           
-           
+
+
         } catch (error) {
             console.log(error);
         }
     }
-    
-    
+
     useEffect(() => {
-
         getResume();
-
     }, []);
 
     return (
         <Fragment>
             <h3>Ultimos 7 dias</h3>
             <div className="cards">
-                <div className="card-single">
-                    <div>
-                        <h1>7</h1>
-                        <span>Tickets Totales</span>
-                    </div>
 
-                </div>
-                <div className="card-single">
-                    <div>
-                        <h1>3</h1>
-                        <span>Tickets en espera</span>
-                    </div>
+                {resume &&
+                    <Fragment>
+                        <div className="card-single">
+                            <div>
+                                <h1>{resume.tickets.total}</h1>
+                                <span>Tickets Totales</span>
+                            </div>
 
-                </div>
+                        </div>
+                        <div className="card-single">
+                            <div>
+                                <h1>{resume.tickets.waiting}</h1>
+                                <span>Tickets en espera</span>
+                            </div>
 
-                <div className="card-single">
-                    <div>
-                        <h1>0</h1>
-                        <span>Tickets resueltos</span>
-                    </div>
+                        </div>
 
-                </div>
+                        <div className="card-single">
+                            <div>
+                                <h1>{resume.tickets.success}</h1>
+                                <span>Tickets resueltos</span>
+                            </div>
 
-                <div className="card-single">
-                    <div>
-                        <h1>{Resume.users}</h1>
-                        <span>Usuarios registrados</span>
-                    </div>
-                </div>
+                        </div>
+
+                        <div className="card-single">
+                            <div>
+                                <h1>{resume.users}</h1>
+                                <span>Usuarios registrados</span>
+                            </div>
+                        </div>
+                    </Fragment>
+                }
             </div>
         </Fragment>
     )
