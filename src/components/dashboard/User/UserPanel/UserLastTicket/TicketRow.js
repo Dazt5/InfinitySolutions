b0e2294566aa5c0d0118d99546f4f62825ede868
David Alvarez <david.alvarez@valid.com>
2021-10-31 21:30:08 -0500

    ft: added resume to User

--- src/components/dashboard/User/UserPanel/UserLastTicket/TicketRow.js
+++ src/components/dashboard/User/UserPanel/UserLastTicket/TicketRow.js
@@ -6,7 +6,7 @@ export const TicketRow = ({ ticket }) => {
 
     return (
         <tr>
-            <td><Link to={`/ticket/${ticket._id}`}>{ticket.subject}</Link></td>
+            <td><Link to={`/ticket/details/${ticket._id}`}>{ticket.subject}</Link></td>
             <td>{ticket.corporation.name}</td>
             <td>{dayjs(ticket.create_at).format('DD/MM/YYYY')}</td>
         </tr>
